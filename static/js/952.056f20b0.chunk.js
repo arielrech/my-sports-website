2420e4ffb4e93e527a3953f52acfcbf7963f3dd4
arielrech <68533206+arielrech@users.noreply.github.com>
2024-09-07 18:16:30 +0300

    Updates

---
+++ static/js/952.056f20b0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksports_tournaments_site=self.webpackChunksports_tournaments_site||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.056f20b0.chunk.js.map
