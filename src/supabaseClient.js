37fc709ecf3311c3219320b24f29ef99342800c6
arielrech <68533206+arielrech@users.noreply.github.com>
2025-05-18 13:35:29 +0300

    Initial commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jcxgljbwxwkfrwgynbat.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjeGdsamJ3eHdrZnJ3Z3luYmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQzMzYwODIsImV4cCI6MjAzOTkxMjA4Mn0.7XC2jXd48mwTe9sX-9m2C0VGICEMeeR6Qcg2g0dv_m0';
+const supabaseUrl = 'https://maijoxtlffhucmhgiapj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haWpveHRsZmZodWNtaGdpYXBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NTcwNzIsImV4cCI6MjA2MzEzMzA3Mn0.esnXuzXUzjfAZKrhXh35D2HoCigwqCwXq5CjQEi2xjs';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+console.log('✅ Supabase URL in use:', supabaseUrl);
 
-export { supabase };
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
